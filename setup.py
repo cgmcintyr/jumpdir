3da0eb45fe4fdb807809702f9ce3947b1fe05618
chrsintyre <chrsintyre@gmail.com>
2015-08-15 18:13:12 +0000

    Added classifiers and further meta-data to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import setup
 
-setup(name='jumpdir',
-      version='0.1',
-      description='A command line tool for quickly navigating your home directory.',
-      url='http://github.com/chrsintyre/jumpdir',
-      author='chrsintyre',
-      author_email='chrsintyre@gmail.com',
-      license='MIT',
-      packages=['jumpdir'],
-      scripts=['scripts/jumpdir'],
-      zip_safe=False
-      )
+setup(
+    name='jumpdir',
+    version='0.1',
+    description='A command line tool for quickly navigating your home directory.',
+    author='chrsintyre',
+    author_email='chrsintyre@gmail.com',
+    url='http://github.com/chrsintyre/jumpdir',
+    zip_safe=False,
 
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ]
+
+    keywords='commandline navigation'
+
+    packages=['jumpdir'],
+    scripts=['scripts/jumpdir'],
+)
