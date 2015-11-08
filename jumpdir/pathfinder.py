4681691f6195ebb0b9f2c32574466ffc482b32ba
Christopher McIntyre <chrsintyre@gmail.com>
2015-11-08 20:10:36 +0000

    Pathfinder docstrings converted to rst

--- jumpdir/pathfinder.py
+++ jumpdir/pathfinder.py
@@ -5,13 +5,23 @@ import os
 
 
 class PathFinder:
-    "Used to check for matches against a given string"
+    """Used to check for matches against an initialised string
+
+    - **parameters** and **instance variables**
+    :param str string: string to compare matches against
+    :ivar str string: copy of value in string param
+
+    """
 
     def __init__(self, string):
         self.string = string
 
     def check_match(self, dname):
-        ""
+        """Check name of directory against string ivar
+
+        - **parameters** and **instance variables**
+        :param str dname: name of directory
+        """
         if dname.lower() == self.string.lower():
             return True
         else:
