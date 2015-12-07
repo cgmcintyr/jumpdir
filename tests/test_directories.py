0037a9d8cce37a00818c9b48e648b5e71a07af76
Christopher McIntyre <chrsintyre@gmail.com>
2015-12-07 18:06:17 +0000

    Refactored directories tests

--- tests/test_directories.py
+++ tests/test_directories.py
@@ -31,7 +31,7 @@ class MainTest(unittest.TestCase):
         os.mkdir('mock_dtree')
         cls.test_path = os.path.join(cls.base_path, 'mock_dtree')
         create_dtree(simple_dtree, cls.test_path)
-
+        cls.directories = Directories(cls.test_path)
         try:
             # Python2
             cls.assertCountEqual = cls.assertItemsEqual
@@ -44,49 +44,41 @@ class MainTest(unittest.TestCase):
         shutil.rmtree(cls.test_path)
 
     def test_Directories_initialises_with_base_dir_attribute_of_type_str(self):
-        ddict = Directories(self.test_path)
-
-        self.assertEqual(type(ddict.base_dir), str)
+        self.assertEqual(type(self.directories.base_dir), str)
 
     def test_Directories_initialises_with_correct_base_dir_attribute(self):
-        ddict = Directories(self.test_path)
+        self.assertEqual(self.directories.base_dir, self.test_path)
 
-        self.assertEqual(ddict.base_dir, self.test_path)
-
-    def test_Directories_initialises_with_dirs_attribute_of_type_list(self):
-        ddict = Directories(self.test_path)
-        self.assertEqual(type(ddict.dirs), defaultdict)
+    def test_Directories_initialises_with_dirs_attribute_of_type_default_dict(self):
+        self.assertEqual(type(self.directories.dirs), defaultdict)
 
     def test_Directories_is_iterable(self):
-        ddict = Directories(self.test_path)
-
-        self.assertCountEqual(iter(ddict), ddict.dirs.keys())
+        self.assertCountEqual(iter(self.directories), self.directories.dirs.keys())
 
     def test_dict_builder_method_returns_dict_of_correct_length(self):
-        dirs = Directories(self.test_path).dirs
-
-        self.assertEqual(len(dirs), 6)
+        self.assertEqual(len(self.directories.dirs), 6)
 
     def test_dict_builder_stores_directories_with_matching_names_under_same_key(self):
-        dirs = Directories(self.test_path).dirs
-
-        self.assertEqual(len(dirs['example']), 2)
+        self.assertEqual(len(self.directories.dirs['example']), 2)
 
     def test_dict_builder_stores_paths(self):
-        dirs = Directories(self.test_path).dirs
-
         self.assertIn(
             os.path.join(self.test_path, 'first', 'example'),
-            dirs['example'])
+            self.directories.dirs['example'])
 
     def test_dict_builder_ignores_directories_beginning_with_a_period(self):
-        dirs = Directories(self.test_path).dirs
-
-        self.assertNotIn('.hidden', dirs.keys())
+        self.assertNotIn('.hidden', self.directories.dirs.keys())
 
     def test_Directories_instances_are_subscriptable(self):
-        ddict = Directories(self.test_path)
-        self.assertEqual(ddict['example'], ddict.dirs['example'])
+        self.assertEqual(self.directories['example'], self.directories.dirs['example'])
+
+    def test_shallowest_path_to_returns_shortest_path_from_list(self):
+        shallowest = self.directories.shallowest_path_to('example')
+        is_shallowest = True
+        for path in self.directories['example']:
+            if (shallowest > path):
+                is_shallowest = False
+        self.assertTrue(is_shallowest)
 
 if __name__ == '__main__':
     unittest.main()
