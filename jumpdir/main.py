4aeb489046d4baf60cd06a3330159dc466f61e5c
Christopher McIntyre <chrsintyre@gmail.com>
2016-05-07 21:54:24 +0100

    Refactored main function

--- jumpdir/main.py
+++ jumpdir/main.py
@@ -11,12 +11,11 @@ import sys
 
 HOME = os.getenv('HOME')
 DATA_DIR = os.path.join(HOME, '.jumpdir')
-BOOKMARKS  = os.path.join(DATA_DIR, '.jdbookmarks.json')
+BOOKMARKS = os.path.join(DATA_DIR, '.jdbookmarks.json')
 CACHE_FILE = os.path.join(DATA_DIR, '.jdcache.json')
 
 if not os.path.isdir(DATA_DIR):
     os.mkdir(DATA_DIR)
-
 
 def parse_args(args):
     """Parse list/tuple of arguments with argparse module
@@ -26,7 +25,8 @@ def parse_args(args):
         :returns namespace: parsed arguments
     """
     parser = argparse.ArgumentParser(description='jumpdir')
-    subparsers = parser.add_subparsers(help='sub-command help', dest='commands')
+    subparsers = parser.add_subparsers(help='sub-command help',
+                                       dest='commands')
 
     # jumpdir search ...
     parser_search = subparsers.add_parser('search',
@@ -39,8 +39,7 @@ def parse_args(args):
     parser_add = subparsers.add_parser('add', help='add bookmark')
     parser_add.add_argument('name', help='name of bookmark to add')
     parser_add.add_argument('-p', '--path', default=os.getcwd(),
-                            help="define path that bookmark points to"
-                            )
+            help="define path that bookmark points to")
 
     # jumpdir delete ...
     parser_delete = subparsers.add_parser('delete', help='delete bookmark')
@@ -52,38 +51,16 @@ def parse_args(args):
     return parser.parse_args(args)
 
 
-def main(argv=sys.argv[1:]):
-    if len(argv) > 0:
-        args = parse_args(argv)
-    else:
-        raise ValueError("error: no commands given")
-    bm = Bookmarks(BOOKMARKS)
-
-    # Sub command logic
-    if not args.commands:
-        raise ValueError("error: command not recognised")
-    elif args.commands == 'add':
-        bm.add_bookmark(args.name, args.path)
-        return
-    elif args.commands == 'delete':
-        bm.del_bookmark(args.name)
-        return
-    elif args.commands == 'list':
-        bm.list_bookmarks()
-        return
-    elif args.commands == 'search':
-        pass
-
+def run_search(search_term):
     if args.search_term == HOME:
         return HOME
 
-    search_term = args.search_term
     pfinder = PathFinder(search_term)
 
     # Search through bookmarks
-    for dname in bm:
+    for dname in bookmarks:
         if pfinder.check_match(dname):
-            return bm[dname]
+            return bookmarks[dname]
 
     # Search cached dirs
     ddict = Directories(HOME, CACHE_FILE)
@@ -96,6 +73,28 @@ def main(argv=sys.argv[1:]):
     for dname in ddict:
         if pfinder.check_match(dname):
             return ddict.shallowest_path_to(dname)
+
+
+def main(argv=sys.argv[1:]):
+    if len(argv) < 1:
+        raise ValueError("error: no commands given")
+    args = parse_args(argv)
+    bookmarks = Bookmarks(BOOKMARKS)
+
+    # Sub command logic
+    if not args.commands:
+        raise ValueError("error: command not recognised")
+    elif args.commands == 'add':
+        bookmarks.add_bookmark(args.name, args.path)
+        raise SystemExit
+    elif args.commands == 'delete':
+        bookmarks.del_bookmark(args.name)
+        raise SystemExit
+    elif args.commands == 'list':
+        bookmarks.list_bookmarks()
+        raise SystemExit
+    else:
+        return run_search(args.search_term)
 
 
 if __name__ == '__main__':
