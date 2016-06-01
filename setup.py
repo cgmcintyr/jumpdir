004799db166f37dd4f14c6edf7056c0832103cae
Christopher McIntyre <chrsintyre@gmail.com>
2016-06-01 19:41:40 +0100

    Cleaned up setup, added travis-ci config

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='jumpdir',
-    version='0.7.5',
+    version='0.7.6',
     description='A command line tool for quickly navigating your home directory.',
     author='cgmcintyr',
     author_email='me@cgmcintyre.com',
-    url='http://github.com/chrsintyre/jumpdir',
+    url='http://github.com/cgmcintyr/jumpdir',
     zip_safe=False,
     license='MIT',
     classifiers=[
@@ -16,13 +16,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX',
     ],
-    keywords='commandline navigation',
-    packages=['jumpdir'],
+    keywords=['commandline', 'navigation', 'cd', 'directories'],
+    packages=['jumpdir', ],
     scripts=['scripts/jumpdir', 'scripts/jumpdir-search'],
-    install_requires=[
-        'pychalk',
-    ],
-    test_suite='tests',
+    install_requires=['pychalk', ],
+    extra_require={'dev': ['tox']},
 )
